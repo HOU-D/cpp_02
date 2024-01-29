d2a9437c77d7de3759e76f1502862fcfb2bda6ee
AKOUMI HOUDA <93216637+HOU-D@users.noreply.github.com>
2024-01-29 16:52:41 +0100

    Update Fixed.cpp

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -10,3 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+const int Fixed::fractional_bits = 8;
+
+Fixed& Fixed::operator = (const Fixed& other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->value = other.getRawBits();
+    return (*this);
+}
+
+Fixed::Fixed() : value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (value);
+}
+
+void    Fixed::setRawBits( int const raw )
+{
+    value = raw;
+}
