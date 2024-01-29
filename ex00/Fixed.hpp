e2a3497a8abe2a2a611fac02e535859df3acc561
AKOUMI HOUDA <93216637+HOU-D@users.noreply.github.com>
2024-01-29 16:48:21 +0100

    Update Fixed.hpp

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+    private:
+
+        int value;
+        static const int fractional_bits;
+
+    public:
+
+        Fixed();
+        Fixed(const Fixed& other);
+        Fixed& operator = (const Fixed& other);
+        ~Fixed();
+
+        int     getRawBits( void ) const;
+        void    setRawBits( int const raw );
+};
+
+#endif
