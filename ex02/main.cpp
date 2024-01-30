41fc012f3b02bc1cacee4bc0b07bcc0cb4063f3c
AKOUMI HOUDA <93216637+HOU-D@users.noreply.github.com>
2024-01-30 11:01:23 +0100

    Update main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -10,3 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+
+    std::cout << b << std::endl;
+
+    std::cout << Fixed::max( a, b ) << std::endl;
+
+    std::cout << std::endl;
+    // comparison operators:
+    std::cout << "(a > b) = " << (a > b) << std::endl;
+    std::cout << "(a < b) = " << (a < b) << std::endl;
+    std::cout << "(a >= b) = " << (a >= b) << std::endl;
+    std::cout << "(a <= b) = " << (a <= b) << std::endl;
+    std::cout << "(a == b) = " << (a == b) << std::endl;
+    std::cout << "(a != b) = " << (a != b) << std::endl;
+    // arithmetic operators:
+    std::cout << "(a + b) = " << (a + b) << std::endl;
+    std::cout << "(a - b) = " << (a - b) << std::endl;
+    std::cout << "(a * b) = " << (a * b) << std::endl;
+    std::cout << "(a / b) = " << (a / b) << std::endl;
+    return (0);
+}
