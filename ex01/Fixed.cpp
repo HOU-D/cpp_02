e52ea7b87ee8f4f1107f18ba724ca23693596abd
AKOUMI HOUDA <93216637+HOU-D@users.noreply.github.com>
2024-01-30 10:54:42 +0100

    Update Fixed.cpp

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -10,3 +10,55 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedValue(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int intValue) : fixedValue(intValue << fractionalBits)
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float floatValue) : fixedValue(roundf(floatValue * (1 << fractionalBits)))
+{
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other) : fixedValue(other.fixedValue)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator = (const Fixed& other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        fixedValue = other.fixedValue;
+    }
+    return *this;
+}
+
+float Fixed::toFloat() const
+{
+    return (static_cast<float>(fixedValue) / (1 << fractionalBits));
+}
+
+int Fixed::toInt() const
+{
+    return fixedValue >> fractionalBits;
+}
+
+std::ostream& operator << (std::ostream& os, const Fixed& fixed)
+{
+    os << fixed.toFloat();
+    return os;
+}
